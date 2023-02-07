ac0ce6463fbd374db839325fbee7a4540891ad68
Danil <linalone17@gmail.com>
2023-02-07 12:16:04 +0500

    another

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import {DateField} from "./DateField";
+import styles from './App.module.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App () {
+
+    return (
+        <div className={styles.App}>
+            <div>Выберите дату</div>
+            <DateField/>
+        </div>
+    )
 }
-
-export default App;
