bf601f13edf8001f64c311eb3d388e51fc974e2d
Danil <linalone17@gmail.com>
2023-02-20 23:46:49 +0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {DateField} from "./ui/components/DateField";
 import styles from './App.module.scss';
 
 export default function App () {
+    const [width, setWidth] = useState<string>('400');
+    const [height, setHeight] = useState<string>('60');
+
+    function handleWidthChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (!event.target.value.match(/^[0-9]{0,4}$/)) return;
+
+        setWidth(event.currentTarget.value)
+    }
+
+    function handleHeightChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (!event.target.value.match(/^[0-9]{0,4}$/)) return;
+
+        setHeight(event.currentTarget.value)
+    }
 
     return (
         <div className={styles.App}>
-            <h2>wheels</h2>
-            <h3>standard</h3>
+            <h1>Wheels</h1>
+            <div className={styles.mark}>
+                <div>standard</div>
+            </div>
             <DateField initialDate={new Date()}
                        dateFrom={new Date(2022, 4, 14)}
                        dateTo={new Date(2024, 5, 6)}
                        variant={'wheels'}/>
-            <h3>less width</h3>
+            <div className={styles.mark}>
+                <div>short / less width</div>
+            </div>
             <DateField initialDate={new Date()}
                        variant={'wheels'}
                        sizes={{
@@ -20,7 +38,9 @@ export default function App () {
                            width: 300
                        }}
             />
-            <h3>smaller</h3>
+            <div className={styles.mark}>
+                <div>smaller size</div>
+            </div>
             <DateField initialDate={new Date()}
                        variant={'wheels'}
                        sizes={{
@@ -28,7 +48,33 @@ export default function App () {
                            width: 200
                        }}
             />
-            <h2>calendar</h2>
+            <div className={styles.mark}>
+                <div>Custom size</div>
+            </div>
+            <div className={styles.sizes}>
+                <div className={styles.size}>
+                    <legend>width</legend>
+                    <input type="text"
+                           value={width}
+                           onChange={handleWidthChange}/>
+                </div>
+                <div>x</div>
+                <div className={styles.size}>
+                    <legend>height</legend>
+                    <input type="text"
+                           value={height}
+                           onChange={handleHeightChange}
+                    />
+                </div>
+            </div>
+            <DateField initialDate={new Date()}
+                       variant={'wheels'}
+                       sizes={{
+                           height: Number(height),
+                           width: Number(width)
+                       }}
+            />
+            <h1>calendar(Work In Progress)</h1>
             <DateField initialDate={new Date()} variant={'calendar'}/>
         </div>
     )
